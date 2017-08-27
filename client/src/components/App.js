38e958fe525494c02841699e86eb4bae7df9ce38
David Roussov <david.roussov@hotmail.com>
2017-08-27 18:22:38 +1000

    added react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import LandingPage from './LandingPage';
+import Login from './Login';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <LandingPage/>
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component={LandingPage}/>
+          <Route path='/login' component={Login}/>
+        </div>
+      </Router>
     );
   }
 }
